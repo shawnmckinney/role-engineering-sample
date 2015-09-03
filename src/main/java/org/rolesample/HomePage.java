cc2cb5c6045e0089463466702743ea92c397cb44
Shawn McKinney <smckinney@apache.org>
2015-09-02 20:37:15 -0500

    prevent unauthN users from loading home page.

--- src/main/java/org/rolesample/HomePage.java
+++ src/main/java/org/rolesample/HomePage.java
@@ -7,6 +7,9 @@ package org.rolesample;
 import org.apache.log4j.Logger;
 import org.apache.wicket.markup.html.basic.Label;
 
+import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
+
 /**
  * This Role Engineering Sample routes traffic here.  It displays list of page links at the top.
  *
@@ -18,6 +21,18 @@ public class HomePage extends WicketSampleBasePage
     private static final Logger LOG = Logger.getLogger( HomePage.class.getName() );
     public HomePage()
     {
+        HttpServletRequest servletReq = (HttpServletRequest)getRequest().getContainerRequest();
+        Principal principal = servletReq.getUserPrincipal();
+        // needed anytime container security checker allows requests in with old cookie (perhaps after server/app restart)::
+        if(principal == null)
+        {
+            LOG.info( "user not logged in, route to login page instead" );
+            // invalidate the session and force the user to log back on:
+            servletReq.getSession().invalidate();
+            getSession().invalidate();
+            setResponsePage( LoginPage.class );
+        }
+
         add(new Label("label1", "You have access to the link(s) above."));
     }
 }
