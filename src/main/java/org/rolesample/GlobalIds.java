2bd1b97e7f3c4e4bf8d5893152872e43b890d873
Shawn McKinney <smckinney@apache.org>
2016-05-11 07:49:32 -0500

    cleanup

--- src/main/java/org/rolesample/GlobalIds.java
+++ src/main/java/org/rolesample/GlobalIds.java
@@ -19,25 +19,10 @@ public class GlobalIds
     public static final String PSWD_FIELD = "pswdField";
     public static final String LOGIN = "login";
 
-    public static final String BUYERPAGE_OBJNAME = "BuyersPage";
-    public static final String SELLERPAGE_OBJNAME = "SellersPage";
-    //public static final String PAGE2_OBJNAME = "com.mycompany.Page2";
-    //public static final String PAGE3_OBJNAME = "com.mycompany.Page3";
-    public static final String BUYERS_PAGE_OBJNAME = "Buyers";
-    public static final String SELLERS_PAGE_OBJNAME = "Sellers";
-
     public static final String PAGE_BUYERS = "BuyersPage";
     public static final String PAGE_BUYERS_LINK = "Buyers Page";
-
     public static final String PAGE_SELLERS = "SellersPage";
     public static final String PAGE_SELLERS_LINK = "Sellers Page";
-
-/*
-    public static final String PAGE_2 = "PAGE2";
-    public static final String BTN_PAGE_2 = "page2";
-    public static final String PAGE_3 = "PAGE3";
-    public static final String BTN_PAGE_3 = "page3";
-*/
 
     public static final String BTN_ITEM_SEARCH = "item.search";
     public static final String BTN_ITEM_BID = "item.bid";
@@ -45,7 +30,6 @@ public class GlobalIds
     public static final String BTN_ITEM_SHIP = "item.ship";
     public static final String BTN_ACCOUNT_CREATE = "account.create";
     public static final String BTN_AUCTION_CREATE = "auction.create";
-
     public static final String BTN_SWITCH_BUYER = "roles.switch.buyer";
     public static final String BTN_SWITCH_SELLER = "roles.switch.seller";
 
